9871cf5e4b1f33c4bacaccc65f48209abb64d9f0
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-01-24 00:02:02 +0100

    new look

---
+++ assets/js/intro.js
@@ -0,0 +1,13 @@
+document.write(`
+
+<center><img class="img_round" src="images/me.jpg" alt="My Photo" style="width:144px;height:155px;"> </center> 
+<p style="text-align: center;font-size:1.3em"> Mohammad Sadil Khan</p>
+<p style="text-align: center;font-style:italic;font-size:0.8em;">Artificial Intelligence <br> Computer Vision<br>
+Generative Models<br> Explainable AI<br>
+</p>
+<p style="text-align: center;font-size:1.4em">
+    <i class="fa fa-map-marker fa_custom"></i> Lyon, France</p>`
+    
+    );
+
+
---
+++ assets/js/recents.js
@@ -0,0 +1,5 @@
+document.write(`
+
+    <h1>Recents</h1>
+
+`);
