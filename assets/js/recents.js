87d382adab44f213e0e2ff892a5162b86e130c90
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-02-06 13:28:51 +0100

    activities column

--- assets/js/recents.js
+++ assets/js/recents.js
@@ -1,5 +1,14 @@
 document.write(`
 
-    <h1>Recents</h1>
+<h3 style="text-align:center;font-size:2em"> Activities </h3>
+<ul>
+    <li>
+        <p> Started my internship in Creatis Lab, Insa Lyon.</p>
+    </li>
+    <li>
+        <p>I finished my exchange semester from KU Leuven.</p>
+    </li>
+</ul>
+
 
 `);
